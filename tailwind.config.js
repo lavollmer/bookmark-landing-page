cbb199bcf97a127199a17abd107491812135afb9
Laura <laura.e.vollmer@gmail.com>
2024-07-24 12:14:29 -0500

    setup the style guide recommendations in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "soft-blue": "hsl(231, 69%, 60%)", // Soft Blue: hsl(231, 69%, 60%)
+        "soft-red": "hsl(0, 94%, 66%)",
+        "grayish-blue": "hsl(229, 8%, 60%)",
+        "very-dark-blue": "hsl(229, 31%, 21%)",
+      },
+      fontFamily: ["rubik", "sans-serif"],
+    },
   },
   plugins: [],
 };

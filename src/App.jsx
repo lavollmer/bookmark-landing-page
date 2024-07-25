ebdb60a0705f2b5fb5894b57ef5be1050cdb6778
Laura <laura.e.vollmer@gmail.com>
2024-07-25 11:41:23 -0500

    adding react router dom for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import "./App.css";
 import Navigation from "./components/Navigation";
 import BookmarkManager from "./components/BookmarkManager";
 import Features from "./components/Features";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <>
       <div>
-        <Navigation />
-        <BookmarkManager />
-        <Features />
+        <BrowserRouter>
+          <Navigation />
+          <Routes>
+            <BookmarkManager path="/" element={<BookmarkManager />} />
+            <Features path="/features" element={<Features />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   );

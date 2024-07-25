ef88fea6abbd7c2b096ea29e5c11c02273b6c233
Laura <laura.e.vollmer@gmail.com>
2024-07-25 10:41:38 -0500

    styling the landing page

--- src/components/BookmarkManager.jsx
+++ src/components/BookmarkManager.jsx
@@ -14,23 +14,23 @@ const BookmarkManager = () => {
         <div className="shape absolute inset-0 z-0 rounded-full"></div>
       </div>
 
-      <div className="flex flex-col items-center justify-center">
-        <h1 className="font-bold text-lg md:text-4xl text-very-dark-blue">
+      <div className="flex flex-col items-center text-center justify-center">
+        <h1 className="flex flex-col items-center p-4 font-bold text-3xl  md:text-4xl text-very-dark-blue">
           A Simple Bookmark Manager
         </h1>
-        <p className="text-grayish-blue text-md md:text-lg">
+        <p className="flex flex-col items-center p-6 text-grayish-blue text-md md:text-lg">
           A clean and simple interface to organize your favourite websites. Open
           a new browser tab and see your sites load instantly. Try it for free.
         </p>
       </div>
-      <div className="flex flex-row items-center justify-evenly">
+      <div className="flex flex-row items-center justify-center mt-2 space-x-2">
         <div>
-          <button className="bg-soft-blue text-white rounded-md text-sm md:text-lg md:py-3 px-2 md:px-4 font-bold">
+          <button className="bg-soft-blue text-white rounded-md text-md py-3 md:text-lg md:py-3 px-2 md:px-4 font-bold">
             Get it on Chrome
           </button>
         </div>
         <div>
-          <button className="bg-grayish-blue text-white rounded-md text-sm md:text-lg md:py-3 px-2 md:px-4 font-bold">
+          <button className="bg-grayish-blue text-white rounded-md text-md md:text-lg py-3 md:py-3 px-2 md:px-4 font-bold">
             Get it on Firefox
           </button>
         </div>

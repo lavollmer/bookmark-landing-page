cae0b19028c0271148751ee42300085fbb257d7e
Laura <laura.e.vollmer@gmail.com>
2024-07-25 07:55:05 -0500

    creating bookmark manager landing page copy and design

--- src/components/BookmarkManager.jsx
+++ src/components/BookmarkManager.jsx
@@ -3,23 +3,29 @@ import illustrationHero from "../assets/illustration-hero.svg";
 
 const BookmarkManager = () => {
   return (
-    <div>
+    <div className="flex flex-col font-rubik">
       <div>
         <img src={illustrationHero} alt="Illustr" />
       </div>
-      <div>
-        <h1>A Simple Bookmark Manager</h1>
-        <p>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="font-bold text-lg md:text-4xl text-very-dark-blue">
+          A Simple Bookmark Manager
+        </h1>
+        <p className="text-grayish-blue text-md md:text-lg">
           A clean and simple interface to organize your favourite websites. Open
           a new browser tab and see your sites load instantly. Try it for free.
         </p>
       </div>
-      <div>
+      <div className="flex flex-row items-center justify-evenly">
         <div>
-          <button>Get it on Chrome</button>
+          <button className="bg-soft-blue text-white rounded-md text-sm md:text-lg md:py-3 px-2 md:px-4 font-bold">
+            Get it on Chrome
+          </button>
         </div>
         <div>
-          <button>Get it on Firefox</button>
+          <button className="bg-grayish-blue text-white rounded-md text-sm md:text-lg md:py-3 px-2 md:px-4 font-bold">
+            Get it on Firefox
+          </button>
         </div>
       </div>
     </div>

6c4d3d9dcdedcfbe8f4ce28d27d9dbbba2f2e344
Laura <laura.e.vollmer@gmail.com>
2024-07-25 12:35:09 -0500

    fixing the bookmarking simple and easy sharing pages

--- src/components/EasySharing.jsx
+++ src/components/EasySharing.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import IllustrationFeatureThree from "../assets/illustration-features-tab-3.svg";
 
 const EasySharing = () => {
   return (
-    <div>EasySharing</div>
-  )
-}
+    <div className="flex flew-row items-center justify-center m-10">
+      <div>
+        <img src={IllustrationFeatureThree} alt="illustration" />
+      </div>
+      <div className="flex flex-col ml-20 space-y-6">
+        <h1 className="flex flex-col font-bold text-3xl  md:text-4xl text-very-dark-blue">
+          Share your bookmarks
+        </h1>
+        <p className="flex flex-col text-grayish-blue text-md md:text-lg">
+          Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.
+        </p>
+        <div>
+          <button className="bg-soft-blue box-shadow rounded-lg text-white font-bold py-2 px-4">
+            More info
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default EasySharing
+export default EasySharing;
--- src/components/SimpleBookmarking.jsx
+++ src/components/SimpleBookmarking.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import IllustrationFeatureOne from "../assets/illustration-features-tab-1.svg";
 
 const SimpleBookmarking = () => {
   return (
-    <div>SimpleBookmarking</div>
-  )
-}
+    <div className="flex flew-row items-center justify-center m-10">
+      <div>
+        <img src={IllustrationFeatureOne} alt="illustration" />
+      </div>
+      <div className="flex flex-col ml-20 space-y-6">
+        <h1 className="flex flex-col font-bold text-3xl  md:text-4xl text-very-dark-blue">
+          Bookmark in one click
+        </h1>
+        <p className="flex flex-col text-grayish-blue text-md md:text-lg">
+          Organize your bookmarks however you like. Our simple drag-and-drop
+          interface gives you complete control over how you manage your
+          favourite sites.
+        </p>
+        <div>
+          <button className="bg-soft-blue box-shadow rounded-lg text-white font-bold py-2 px-4">
+            More info
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SimpleBookmarking
+export default SimpleBookmarking;

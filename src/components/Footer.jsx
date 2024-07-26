bbed89b0f550a35d768b7138eab2875c698cb5ff
Laura <laura.e.vollmer@gmail.com>
2024-07-26 11:13:11 -0500

    styling for desktop version footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,13 +6,13 @@ import { LiaFacebookSquare } from "react-icons/lia";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col items-center justify-center space-y-8 p-10 md:flex-row bg-very-dark-blue font-rubik">
+    <div className="flex flex-col md:flex-row md:space-x-2 items-center md:justify-between justify-center space-y-8 p-10 md:flex-row bg-very-dark-blue font-rubik">
       <div>
         <div className="flex flex-col">
           <img src={Logo} alt="Logo icon" />
         </div>
       </div>
-      <div className="flex flex-col text-white space-y-4">
+      <div className="flex flex-col md:flex-row md:space-x-4 text-white space-y-4">
         <button className=" text-shadow">FEATURES</button>
         <button className=" text-shadow">PRICING</button>
         <button className=" text-shadow">CONTACT</button>

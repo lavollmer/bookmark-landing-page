dd6b04dc6b5513f95c5d4b491e42326e570fcc40
Laura <laura.e.vollmer@gmail.com>
2024-07-27 11:05:46 -0500

    fixing the footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,21 +2,16 @@ import React from "react";
 import Logo from "../assets/logo-bookmark.svg";
 import FacebookIcon from "../assets/icon-facebook.svg";
 import TwitterIcon from "../assets/icon-twitter.svg";
-import { LiaFacebookSquare } from "react-icons/lia";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col md:flex-row md:space-x-2 items-center md:justify-between justify-center space-y-8 p-10 md:flex-row bg-very-dark-blue font-rubik">
-      <div>
-        <div className="flex flex-col">
-          <img src={Logo} alt="Logo icon" />
-        </div>
-      </div>
-      <div className="flex flex-col md:flex-row md:space-x-4 text-white space-y-4">
-        <button className=" text-shadow">FEATURES</button>
-        <button className=" text-shadow">PRICING</button>
-        <button className=" text-shadow">CONTACT</button>
-      </div>
+    <div className="flex flex-col p-4 items-center text-white font-rubik justify-center md:flex-row md:justify-between space-y-8 bg-very-dark-blue font-rubik">
+      <img src={Logo} alt="Logo icon" />
+      {/* <div className="flex flex-col md:flex-row md:space-x-1 text-white space-y-4"> */}
+      <button>FEATURES</button>
+      <button>PRICING</button>
+      <button>CONTACT</button>
+      {/* </div> */}
       <div className="flex flex-row space-x-2">
         <img src={FacebookIcon} alt="facebook icon" />
         <img src={TwitterIcon} alt="twitter" />

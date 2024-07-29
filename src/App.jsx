38926b3f3a413058f43d7113ffaf4c4ca599bbcf
Laura <laura.e.vollmer@gmail.com>
2024-07-29 11:17:20 -0500

    updating App jsx with all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import "./App.css";
 import Navigation from "./components/Navigation";
-import BookmarkManager from "./components/BookmarkManager";
 import Features from "./components/Features";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Extension from "./components/Extension";
+import Footer from "./components/Footer";
+import FAQ from "./components/FAQ";
+import Newsletter from "./components/Newsletter";
+import BookmarkManager from "./components/BookmarkManager";
 
 function App() {
   return (
     <>
       <div>
-        <BrowserRouter>
-          <Navigation />
-          <Routes>
-            <Route path="/" element={<BookmarkManager />} />
-            <Route path="/features" element={<Features />} />
-          </Routes>
-        </BrowserRouter>
+        <Navigation />
+        <BookmarkManager />
+        <Features />
+        <Extension />
+        <FAQ />
+        <Newsletter />
+        <Footer />
       </div>
     </>
   );
--- src/components/BookmarkManager.jsx
+++ src/components/BookmarkManager.jsx
@@ -1,60 +1,38 @@
 import React from "react";
 import illustrationHero from "../assets/illustration-hero.svg";
 import "../styles/Blueshape.css";
-import Features from "./Features";
-import Extension from "./Extension";
-import Footer from "./Footer";
-import FAQ from "./FAQ";
-import Newsletter from "./Newsletter";
+
 import "../App.css";
 
 const BookmarkManager = () => {
   return (
     <div className="flex flex-col font-rubik">
-      <div className="flex md:flex-row">
+      <div>
+        <img src={illustrationHero} alt="Illustration" />
+      </div>
+      <div className="flex flex-row">
+        <div className="flex flex-col items-center text-center justify-center md:m-10">
+          <h1 className="flex flex-col items-center p-4 font-bold text-3xl  md:text-4xl text-very-dark-blue">
+            A Simple Bookmark Manager
+          </h1>
+          <p className="flex flex-col md:flex-row items-center p-6 text-grayish-blue text-md md:text-lg">
+            A clean and simple interface to organize your favourite websites.
+            Open a new browser tab and see your sites load instantly. Try it for
+            free.
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-row items-center justify-center mt-2 space-x-4 font-rubik">
         <div>
-          <img src={illustrationHero} alt="Illustration" />
-          {/* <div className="shape absolute inset-0 z-0 rounded-full"></div> */}
+          <button className="box-shadow bg-soft-blue text-white rounded-md text-md py-3 md:text-lg md:py-3 px-2 md:px-4 ">
+            Get it on Chrome
+          </button>
         </div>
-        <div className="flex flex-row">
-          <div className="flex flex-col items-center text-center justify-center md:m-10">
-            <h1 className="flex flex-col items-center p-4 font-bold text-3xl  md:text-4xl text-very-dark-blue">
-              A Simple Bookmark Manager
-            </h1>
-            <p className="flex flex-col md:flex-row items-center p-6 text-grayish-blue text-md md:text-lg">
-              A clean and simple interface to organize your favourite websites.
-              Open a new browser tab and see your sites load instantly. Try it
-              for free.
-            </p>
-          </div>
+        <div>
+          <button className="box-shadow bg-customGray text-very-dark-blue rounded-md font-bold text-md md:text-lg py-3 md:py-3 px-2 md:px-4 ">
+            Get it on Firefox
+          </button>
         </div>
-        <div className="flex flex-row items-center justify-center mt-2 space-x-4 font-rubik">
-          <div>
-            <button className="box-shadow bg-soft-blue text-white rounded-md text-md py-3 md:text-lg md:py-3 px-2 md:px-4 ">
-              Get it on Chrome
-            </button>
-          </div>
-          <div>
-            <button className="box-shadow bg-customGray text-very-dark-blue rounded-md font-bold text-md md:text-lg py-3 md:py-3 px-2 md:px-4 ">
-              Get it on Firefox
-            </button>
-          </div>
-        </div>
-      </div>
-      <div>
-        <Features />
-      </div>
-      <div>
-        <Extension />
-      </div>
-      <div>
-        <FAQ />
-      </div>
-      <div>
-        <Newsletter />
-      </div>
-      <div>
-        <Footer />
       </div>
     </div>
   );
